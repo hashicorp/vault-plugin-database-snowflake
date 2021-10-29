a9a5e3932ce48cc6bacf861abd6ff26d50bc4280
Vinay Gopalan <vinay@hashicorp.com>
2021-10-29 09:32:08 -0700

    set env var back to 2s

--- snowflake_test.go
+++ snowflake_test.go
@@ -459,7 +459,7 @@ func attemptDropUser(connString, username string) {
 func getRequestTimeout(t *testing.T) time.Duration {
 	rawDur := os.Getenv("VAULT_TEST_DATABASE_REQUEST_TIMEOUT")
 	if rawDur == "" {
-		return 10 * time.Second
+		return 2 * time.Second
 	}
 
 	dur, err := time.ParseDuration(rawDur)
